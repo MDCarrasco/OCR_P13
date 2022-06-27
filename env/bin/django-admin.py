fa201bd75e3ff01168452b8cade95655fc53464a
MDCarrasco <faultyquisby@protonmail.com>
2022-06-27 14:08:14 +0200

    first commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/faultyquisby/openclassrooms/PythonFormation/livrables1/P13_carrasco_michael/Python-OC-Lettings-FR/env/bin/python3.9
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
