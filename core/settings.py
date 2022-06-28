7be4157e5c2ec05ce5a16722181aa1262aa57fee
MDCarrasco <faultyquisby@protonmail.com>
2022-06-28 15:57:08 +0200

    [migration] migration in config.yml 13

--- core/settings.py
+++ core/settings.py
@@ -66,9 +66,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config()
-}
+ON_HEROKU = False
+if 'HEROKU' in os.environ:
+    ON_HEROKU = True
+
+if ON_HEROKU:
+    DATABASE_URL = 'postgresql://<postgresql>'
+else:
+    DATABASE_URL = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 
 
 # Password validation
